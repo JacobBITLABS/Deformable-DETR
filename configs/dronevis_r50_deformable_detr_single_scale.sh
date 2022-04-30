102af5dd1d402ce6588ff8af882ac16df1dbe663
Jacob Nielsen <jacob.nielsen111@gmail.com>
2022-04-30 16:27:26 +0200

    python3 exe

--- configs/dronevis_r50_deformable_detr_single_scale.sh
+++ configs/dronevis_r50_deformable_detr_single_scale.sh
@@ -5,7 +5,7 @@ set -x
 EXP_DIR=exps/r50_deformable_detr_single_scale
 PY_ARGS=${@:1}
 
-python -u main.py \
+python3 -u main.py \
     --num_feature_levels 1 \
     --output_dir ${EXP_DIR} \
     --dataset_file configs/dronevis \
