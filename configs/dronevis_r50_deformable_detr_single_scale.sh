d238dccdeaac669d0b064c8f96469e5bbd5fa6f6
Jacob Nielsen <jacob.nielsen111@gmail.com>
2022-04-30 16:15:16 +0200

    dronevis config added

---
+++ configs/dronevis_r50_deformable_detr_single_scale.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -x
+
+EXP_DIR=exps/r50_deformable_detr_single_scale
+PY_ARGS=${@:1}
+
+python -u main.py \
+    --num_feature_levels 1 \
+    --output_dir ${EXP_DIR} \
+    --dataset_file configs/dronevis \
+    --data_path ../dronevis_dataset/ \
+    ${PY_ARGS}
