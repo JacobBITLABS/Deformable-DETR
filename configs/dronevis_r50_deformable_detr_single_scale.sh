04c427ada114136bf9871dc2b6ae370362b11a22
Jacob Nielsen <jacob.nielsen111@gmail.com>
2022-05-10 10:54:50 +0200

    50 epochs

--- configs/dronevis_r50_deformable_detr_single_scale.sh
+++ configs/dronevis_r50_deformable_detr_single_scale.sh
@@ -2,13 +2,13 @@
 
 set -x
 
-EXP_DIR=exps4/r50_deformable_detr_single_scale
+EXP_DIR=exps/test2/r50_deformable_detr_single_scale
 PY_ARGS=${@:1}
 
 python3 -u main.py \
     --num_feature_levels 1 \
     --output_dir ${EXP_DIR} \
     --dataset_file dronevis \
-    --epochs 500 \
+    --epochs 50 \
     --data_path ../dronevis_dataset/ \
     ${PY_ARGS}
